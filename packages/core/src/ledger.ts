be5091c6de341015b55133d25a8b6f9c57dbd0d9
Sam Ternent <sam@concords.app>
2021-01-24 22:07:33 +0000

    expose interface

--- packages/core/src/ledger.ts
+++ packages/core/src/ledger.ts
@@ -1,2 +1,11 @@
-export { create, mine as commit, is_chain_valid as verify } from './blockchain';
+export {
+  create,
+  mine as commit,
+  is_chain_valid as verify,
+  TransactionBase,
+  Transaction,
+  TransactionHash,
+  Block,
+  Blockchain
+} from './blockchain';
 export { start as load } from './runtime';
