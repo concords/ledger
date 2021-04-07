9146d439f32393710cd0f93e1d73049c7d5b8a0b
Sam Ternent <sam@concords.app>
2021-04-07 14:41:59 +0100

    adds user spinner

--- packages/vue-kit/src/App.vue
+++ packages/vue-kit/src/App.vue
@@ -40,13 +40,30 @@
         </li>
       </ul>
       <div
-        class="flex cursor-pointer hover:text-gray-300"
+        v-if="!userLoading"
+        class="flex cursor-pointer hover:text-gray-300 text-white"
         @click="generateUser"
       >
         <img
           :src="`https://robohash.org/${user.identity.x}${user.identity.y}`"
           class="inline-block bg-white w-12 h-12 ring-white shadow rounded-full ml-4 border border-gray-400 my-2"
         >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 my-5 mx-2"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+          />
+        </svg>
+      </div>
+      <div v-else>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 my-5 mx-2 text-white"
@@ -58,7 +75,7 @@
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
           />
         </svg>
       </div>
@@ -86,17 +103,20 @@ export default defineComponent({
   setup() {
     const ledger = ref(JSON.parse(localStorage.getItem('ledger')));
     const user = ref(JSON.parse(localStorage.getItem('user')) || defaultUser);
+    const userLoading = ref(false);
 
     watch(ledger, () => {
       localStorage.setItem('ledger', JSON.stringify(ledger.value));
     })
 
     async function generateUser() {
+      userLoading.value = true;
       user.value = await createIdentity();
       localStorage.setItem('user', JSON.stringify(user.value));
+      userLoading.value = false;
     }
 
-    return { ledger, user, generateUser }
+    return { ledger, user, generateUser, userLoading }
   },
 })
 </script>
