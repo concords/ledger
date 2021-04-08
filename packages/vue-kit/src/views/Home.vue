8e03d5358f7caa397069dcd0f07108ec2f861b9a
Sam Ternent <sam@concords.app>
2021-04-08 15:40:01 +0100

    copy

--- packages/vue-kit/src/views/Home.vue
+++ packages/vue-kit/src/views/Home.vue
@@ -7,11 +7,11 @@
       Vue 3 Serverless Todo App
     </h2>
     <p>
-      Demo app for
+      A demo app for
       <a
         class="text-blue-500 hover:text-blue-600"
         href="https://www.npmjs.com/package/@concords/ledger"
-      >@concords/ledger</a>
+      >@concords/ledger</a> library
     </p>
     <p class="my-4">
       Serverless authentication provided through
@@ -22,6 +22,13 @@
     </p>
     <p class="my-6 text-gray-400 text-xs font-thin">
       not open-source (yet), I have a few deployment tokens to automate first
+    </p>
+    <p class="text-gray-500 pt-10 text-sm">
+      A follow up project to
+      <a
+        class="text-blue-500 hover:text-blue-600"
+        href="https://open.concords.app"
+      >http://open.concords.app</a>.
     </p>
   </div>
 </template>
